e874dcb8ee67cc9c3cd1b0d2a5c04f8a0f0ffc4c
yuizho <yuizho3@gmail.com>
2019-04-04 22:13:57 +0900

    add validation to controller

--- src/main/java/io/github/yuizho/mybatisdemo/controller/view/CityViewController.java
+++ src/main/java/io/github/yuizho/mybatisdemo/controller/view/CityViewController.java
@@ -4,16 +4,28 @@ import io.github.yuizho.mybatisdemo.mybatis.CityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Controller
+@Validated
 public class CityViewController {
     @Autowired
     CityMapper cityMapper;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/greeting/{name}")
+    public String greeting(@NotNull
+                               @Size(min = 1, max = 10)
+                               @Pattern(regexp = "^[a-zA-Z]+$")
+                               @PathVariable(name="name") String name,
+                           Model model) {
         model.addAttribute("name", name);
         model.addAttribute("city", cityMapper.findById(1L).toString());
         return "greeting";
