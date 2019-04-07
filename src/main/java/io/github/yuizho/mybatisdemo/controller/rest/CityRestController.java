88b80848d436906159546844def7b27cf9fcfb5b
yuizho <yuizho3@gmail.com>
2019-04-07 15:39:13 +0900

    add post endpoint

--- src/main/java/io/github/yuizho/mybatisdemo/controller/rest/CityRestController.java
+++ src/main/java/io/github/yuizho/mybatisdemo/controller/rest/CityRestController.java
@@ -2,11 +2,12 @@ package io.github.yuizho.mybatisdemo.controller.rest;
 
 import io.github.yuizho.mybatisdemo.mybatis.CityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class CityRestController {
     @Autowired
     CityMapper cityMapper;
@@ -15,4 +16,7 @@ public class CityRestController {
     public String city() {
         return cityMapper.findById(1L).toString();
     }
+
+    @PostMapping("/post")
+    public void post() { System.out.println("hello!!"); }
 }
