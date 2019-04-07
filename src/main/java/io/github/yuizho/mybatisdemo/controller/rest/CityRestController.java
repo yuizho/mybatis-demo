46952cb8b90a42ed86352a8746c6a94a2ca8227d
yuizho <yuizho3@gmail.com>
2019-04-07 16:16:27 +0900

    add created parameter(which is Date type) to post endpoint

--- src/main/java/io/github/yuizho/mybatisdemo/controller/rest/CityRestController.java
+++ src/main/java/io/github/yuizho/mybatisdemo/controller/rest/CityRestController.java
@@ -2,9 +2,14 @@ package io.github.yuizho.mybatisdemo.controller.rest;
 
 import io.github.yuizho.mybatisdemo.mybatis.CityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +23,12 @@ public class CityRestController {
     }
 
     @PostMapping("/post")
-    public void post() { System.out.println("hello!!"); }
+    public void post(@RequestParam(value = "created", required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd") Date created) {
+        if (created == null) {
+            created = new Date();
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        System.out.println(sdf.format(created));
+    }
 }
